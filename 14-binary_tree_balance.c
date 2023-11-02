934f0a841b8bea0b81e1d444d6c53292286635e4
Anyoh98 <fonachuanyoh@yahoo.com>
2023-11-03 00:34:50 +0100

    tesing

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,10 +6,10 @@
  * Return: height
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height_b(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
 	{
@@ -19,13 +19,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			left_height = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			right_height = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
 		}
-		return ((left_height > right_height) ? left_height : right_height);
+		return ((l > r) ? l : r);
 	}
 }
-
 /**
  * binary_tree_balance - function that measures the balance factor of a
  * binary tree
@@ -39,8 +38,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left = ((int)binary_tree_height(tree->left));
-		right = ((int)binary_tree_height(tree->right));
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
 		total = left - right;
 	}
 	return (total);
